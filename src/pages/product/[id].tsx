0f2d57a2dacc7ddb8f3b9b76e03a676b1f31916e
guivicentep <guivicente.gvp@hotmail.com>
2023-07-03 15:29:21 -0300

    feat: Estrutura da página de produto

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,7 +1,30 @@
-import { useRouter } from 'next/router'
+// import { useRouter } from 'next/router'
+import {
+  ImageContainer,
+  ProductContainer,
+  ProductDetails,
+} from '../../styles/pages/product'
 
 export default function Product() {
-  const { query } = useRouter()
+  // const { query } = useRouter()
 
-  return <h1>Product {JSON.stringify(query)}</h1>
+  return (
+    <ProductContainer>
+      <ImageContainer></ImageContainer>
+
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+        <span>R$ 79,90</span>
+
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Placeat
+          officiis corporis magni sequi aut accusantium mollitia, minima fugit
+          itaque tempora, esse tempore quia. Voluptas dignissimos cupiditate,
+          molestias necessitatibus facere harum?
+        </p>
+
+        <button>Comprar agora</button>
+      </ProductDetails>
+    </ProductContainer>
+  )
 }
